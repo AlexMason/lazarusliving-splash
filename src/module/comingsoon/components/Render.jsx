8ca12a30172773a90985fba1024fdd5bb55c5835
Alexander Mason <aemason93@gmail.com>
2021-10-26 22:58:19 -0400

    links working now

--- src/module/comingsoon/components/Render.jsx
+++ src/module/comingsoon/components/Render.jsx
@@ -14,6 +14,7 @@ export default function Render() {
       <title>Lazarus Living</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
+
     <div className="bg-white p-10 w-[40rem] pb-5">
       <div className="">
         {/* <Image src={LogoImage} layout="intrinsic" width={`${4741/imageScale}px`} height={`${1997/imageScale}px`} /> */}
@@ -24,7 +25,7 @@ export default function Render() {
         <p className="text-2xl tracking-wider text-center">Lazarus Living is a community organization dedicated to providing medical services and resources to the vulnerable and underserved homeless populations in the Indianapolis area.</p>
         <hr className="text-gray-900 border-gray-300" />
         <p style={{ "textIndent": "2rem" }}>We currently rely on our volunteers to provide these critcal services to the community.  We have a need for caregivers, medical professionals, educators, chefs, housekeepers, plumbers, electricians, and so much more.</p>
-        <p style={{ "textIndent": "2rem" }}>If you are interested in helping us with our mission, please visit our social media page for <span className="underline">upcoming events</span> in the community, or consider <span className="underline">donating</span> to our cause.</p>
+        <p style={{ "textIndent": "2rem" }}>If you are interested in helping us with our mission, please visit our social media page for <a class="underline" href="https://www.facebook.com/lazarusliving/events">upcoming events</a> in the community, or consider <span className="underline cursor-pointer" onClick={() => setDonateModalOpen(true)}>donating</span> to our cause.</p>
         <div className="text-5xl mx-auto pt-5">
           <a href="https://www.facebook.com/lazarusliving" target="_blank"><FaFacebook className="hover:text-[#4267B2]" /></a>
         </div>
@@ -35,6 +36,7 @@ export default function Render() {
         <p className="text-xs text-center">&copy; Lazarus Living Inc. 2021 | Lazarus Living Inc. is a 501(c)3 organization.</p>
       </div>
     </div>
+
     <DonateModal open={donateModalOpen} close={() => setDonateModalOpen(false)} />
   </div>;
 }
